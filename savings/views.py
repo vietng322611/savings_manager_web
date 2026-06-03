b8600cd3170fef102a664ba950291090e86dfb5b
vietng <vietng322611@gmail.com>
2026-06-03 19:52:19 +0700

    show withdraw/deposit error instead of crashing

--- savings/views.py
+++ savings/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect, render
 from django.db.models import Q
 
 from dashboard.decorators import customer_required
-from dashboard.flash import flash_success
+from dashboard.flash import flash_success, flash_error
 from dashboard.utils import get_parameter
 from savings.forms import (
     SavingPlanCreateForm,
@@ -49,17 +49,23 @@ def saving_plan_detail(request, plan_id):
             if action_form.is_valid():
                 action = action_form.cleaned_data["action"]
                 amount = action_form.cleaned_data["amount"]
-                if action == "deposit":
-                    deposit(saving_plan, amount)
-                    flash_success(request, f"Created request to deposit {amount}")
-                else:
-                    withdraw(saving_plan, amount)
-                    flash_success(request, f"Created request to withdraw {amount}")
+                try:
+                    if action == "deposit":
+                        deposit(saving_plan, amount)
+                        flash_success(request, f"Created request to deposit {amount}")
+                    else:
+                        withdraw(saving_plan, amount)
+                        flash_success(request, f"Created request to withdraw {amount}")
+                except ValueError as exc:
+                    flash_error(request, str(exc))
 
                 return redirect("saving_plan_detail", plan_id=plan_id)
         else:
-            withdraw(saving_plan, saving_plan.balance)
-            flash_success(request, "Created request to withdraw all balances")
+            try:
+                withdraw(saving_plan, saving_plan.balance)
+                flash_success(request, "Created request to withdraw all balances")
+            except ValueError as exc:
+                flash_error(request, str(exc))
             return redirect("saving_plan_detail", plan_id=plan_id)
 
     transactions = saving_plan.transactions.order_by("-timestamp")
