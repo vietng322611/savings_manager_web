34076a4bae88d5a36fc7d37f9e1fee656fb3621e
vietng <vietng322611@gmail.com>
2026-06-04 16:29:46 +0700

    remove `django_browser_reload`

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware'
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
