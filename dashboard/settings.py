a896f068798b1b0c59863d1c4a460dbc23b1ae89
vietng <vietng322611@gmail.com>
2026-06-05 08:18:48 +0700

    update

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -36,6 +36,9 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
