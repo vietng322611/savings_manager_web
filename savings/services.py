635561722abb95206b5ba1391441a64cb66acbb9
vietng <vietng322611@gmail.com>
2026-06-07 13:26:21 +0700

    show `balance_after` same as `balance_before` when canceling transactions

--- savings/services.py
+++ savings/services.py
@@ -404,7 +404,8 @@ def process_transaction(txn: Transaction, new_status: TransactionStatus) -> Tran
 
         if new_status == TransactionStatus.CANCELED:
             txn.status = TransactionStatus.CANCELED
-            txn.save(update_fields=["status"])
+            txn.balance_after = txn.balance_before
+            txn.save(update_fields=["status", "balance_after"])
             if txn.transaction_type == TransactionType.OPEN:
                 saving_plan = txn.saving_plan
                 saving_plan.soft_delete()
